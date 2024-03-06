00feb45723a5deaba937b020f223622020e2eee2
EbenezerOffei <ebenezeroffei@outlook.com>
2024-03-06 17:32:04 +0000

    Feat: 🏴 Basic implementation of qr code reader and scanner complete.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,13 @@ class App {
 
     constructor() {
         this.qrCode;
+        this.qrCodeScanner;
         this.generateQRCode();
         this.clearQrCode();
         this.downloadQrCode();
+        this.initQrCodeScan();
         this.scanQrCode();
+        this.stopQrCodeScanner();
     }
 
     generateQRCode = () => {
@@ -50,27 +53,61 @@ class App {
 
             download.click();
             download.remove();
-
-            // this.qrCode.download(`QR Code ${new Date().getTime()}`)
         }
     }
 
 
-
-    scanQrCode = () => {
-        const qrCodeScanner = new Html5QrcodeScanner(
-            "reader", {
-            fps: 10,
-            qrbox: 250,
+    initQrCodeScan = async () => {
+        if (await QrScanner.hasCamera()) {
+            const cameraList = await QrScanner.listCameras(true);
+            cameraList.forEach(ele => {
+                document.getElementById('select-camera').appendChild(
+                    Object.assign(
+                        document.createElement('option'),
+                        {
+                            value: ele.id,
+                            innerHTML: ele.label
+                        }
+                    )
+                )
+            })
         }
-        );
-        qrCodeScanner.render(
-            (decodedText, decodedResult) => {
-                document.getElementById('decoded-text').innerHTML = decodedText;
-            }, (errorMessage) => {
-                console.log(errorMessage)
-            }
-        )
+    }
+
+    scanQrCode = async () => {
+        document.getElementById('start-scan').onclick = () => {
+            // Show the reader container
+            document.getElementById('reader-container').style.display = 'flex';
+            // Hide the select camera
+            document.getElementById('select-camera-container').style.display = 'none';
+
+            const selectedCamera = document.getElementById('select-camera').value;
+            const videoElement = document.getElementById('reader');
+            this.qrCodeScanner = new QrScanner(
+                videoElement,
+                result => {
+                    document.getElementById('decoded-text').innerHTML = result.data;
+                },
+                {
+                    highlightScanRegion: true,
+                    highlightCodeOutline: true,
+                    preferredCamera: selectedCamera,
+                }
+            );
+            this.qrCodeScanner.start();
+        }
+    }
+
+    stopQrCodeScanner = () => {
+        document.getElementById('stop-scan').onclick = () => {
+            // Hide the reader container
+            document.getElementById('reader-container').style.display = 'none';
+            // Show the select camera
+            document.getElementById('select-camera-container').style.display = 'flex';
+
+            this.qrCodeScanner.destroy();
+            this.qrCodeScanner = null;
+        }
     }
 }
 
